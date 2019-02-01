a24f0b1dbb512d712126ec5d6145358930df75c7
Denis.Sidorov <Denis.Sidorov@altarix.ru>
2019-02-01 11:56:31 +0400

    Обновил редактор сообщений, добавил подсветку кода. Прибил футер к полу.

--- qa-plugin/wysiwyg-editor/ckeditor/config.js
+++ qa-plugin/wysiwyg-editor/ckeditor/config.js
@@ -1,38 +1,38 @@
 /**
- * Q2A custom config for CKEditor
- * NOTE: if you make changes to this file, make sure that you do not overwrite it when upgrading Q2A!
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For complete reference see:
+	// Define changes to default configuration here.
+	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar arrangement, two rows of buttons
-	config.toolbar = [
-		{ name: 'font', items: [ 'Font', 'FontSize', 'Format' ] },
-		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote' ] },
-		{ name: 'align', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
 		'/',
-		{ name: 'basic', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript' ] },
-		{ name: 'color', items: [ 'TextColor', 'BGColor' ] },
-		{ name: 'links', items: [ 'Link', 'Unlink' ] },
-		{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar', 'Smiley' ] },
-		{ name: 'last', items: [ 'Undo', 'Redo', 'RemoveFormat', 'Maximize' ] }
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
 	];
 
-	// Set the most common block elements
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
-	config.entities = false;
 
-	// Make dialogs simpler
-	config.removeDialogTabs = 'image:advanced;link:advanced;table:advanced';
-
-	// Use native spell checking (note: Ctrl+right-click is required for native context menu)
-	config.disableNativeSpellChecker = false;
-
-	// Prevent blank paragraphs
-	config.fillEmptyBlocks = false;
-
-	// Add custom CSS
-	config.contentsCss = [CKEDITOR.getUrl('contents.css'), CKEDITOR.getUrl('contents-custom.css')];
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
